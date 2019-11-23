161da8464781450844f4435d936b516c0e64d94b
Marko <marko.andrushchenko@nure.ua>
2019-11-23 18:29:39 +0200

    web security config

--- src/main/java/com/stardusted1/Sendicate/app/config/WebSecurityConfig.java
+++ src/main/java/com/stardusted1/Sendicate/app/config/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/").permitAll()
+                .mvcMatchers("/api").hasRole("CUSTOMER")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
