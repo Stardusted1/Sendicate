f2c64a612cdbb962653740c3241a9b0462e1e5a9
Stardusted1 <32225453+Stardusted1@users.noreply.github.com>
2019-11-24 18:18:03 +0200

    Update WebSecurityConfig.java

--- src/main/java/com/stardusted1/Sendicate/app/config/WebSecurityConfig.java
+++ src/main/java/com/stardusted1/Sendicate/app/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/","/assets/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
